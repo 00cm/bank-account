e63312e2328d31f2c8d59a70ae65a9dc8184a636
chloe <cm0998382@otc.edu>
2025-01-25 08:59:41 -0600

    Add CheckingAccount Class

--- Accounts/CheckingAccount.cs
+++ Accounts/CheckingAccount.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace BankAccountSim.Accounts
 {
-    internal class CheckingAccount
+    public class CheckingAccount : Account
     {
+        private decimal overdraftLimit;
+
+        public CheckingAccount(string accountHolder, decimal initialBalance, decimal overdraftLimit)
+            : base(accountHolder, initialBalance)
+        {
+            this.overdraftLimit = overdraftLimit;
+        }
+
+        public override void Withdraw(decimal amount)
+        {
+            if (amount > 0 && (Balance + overdraftLimit) >= amount)
+            {
+                Balance -= amount;
+                Console.WriteLine($"Withdrew ${amount}. New balance: ${Balance}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid withdrawal amount or overdraft limit exceeded.");
+            }
+        }
     }
 }
